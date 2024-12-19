a8687e0039bdb1f089af6f148049a22a9a9058b6
JasonYang <yjx723@users.noreply.github.com>
2024-12-19 23:12:19 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,88 @@
+# encoding:utf-8
+
+import os
+import signal
+import sys
+import time
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+from channel import channel_factory
+from common import const
+from config import load_config
+from plugins import *
+
+
+def sigterm_handler_wrap(_signo):
+    old_handler = signal.getsignal(_signo)
+
+    def func(_signo, _stack_frame):
+        logger.info("signal {} received, exiting...".format(_signo))
+        conf().save_user_datas()
+        if callable(old_handler):  #  check old_handler
+            return old_handler(_signo, _stack_frame)
+        sys.exit(0)
+
+    signal.signal(_signo, func)
+
+
+def start_channel(channel_name: str):
+    channel = channel_factory.create_channel(channel_name)
+    if channel_name in ["wx", "wxy", "terminal", "wechatmp", "web", "wechatmp_service", "wechatcom_app", "wework",
+                        const.FEISHU, const.DINGTALK]:
+        PluginManager().load_plugins()
+
+    if conf().get("use_linkai"):
+        try:
+            from common import linkai_client
+            threading.Thread(target=linkai_client.start, args=(channel,)).start()
+        except Exception as e:
+            pass
+    channel.startup()
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
+
+
+def start_http_server():
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    httpd.serve_forever()
+
+
+def run():
+    try:
+        # load config
+        load_config()
+        # ctrl + c
+        sigterm_handler_wrap(signal.SIGINT)
+        # kill signal
+        sigterm_handler_wrap(signal.SIGTERM)
+
+        # create channel
+        channel_name = conf().get("channel_type", "wx")
+
+        if "--cmd" in sys.argv:
+            channel_name = "terminal"
+
+        if channel_name == "wxy":
+            os.environ["WECHATY_LOG"] = "warn"
+
+        # Start the HTTP server in a separate thread
+        threading.Thread(target=start_http_server, daemon=True).start()
+
+        start_channel(channel_name)
+
+        while True:
+            time.sleep(1)
+    except Exception as e:
+        logger.error("App startup failed!")
+        logger.exception(e)
+
+
+if __name__ == "__main__":
+    run()
